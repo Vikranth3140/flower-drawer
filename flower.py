fe1136d3086f909728677d5a61ba56e3562c2ca1
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-02-23 05:42:39 +0530

    Adding a message feature

--- flower.py
+++ flower.py
@@ -1,15 +1,30 @@
 import turtle
 
-t = turtle.Turtle()
-s = turtle.Screen()
+def draw_flower(name, message):
+    t = turtle.Turtle()
+    s = turtle.Screen()
+    s.bgcolor("black")
+    t.pensize(2)
+    colors = ["purple", "blue", "green", "yellow", "orange"]
 
-s.bgcolor("black")
-t.pencolor("pink")
+    t.speed(0)
+    for i in range(150):
+        t.pencolor(colors[i % len(colors)])
+        t.circle(190 - i, 90)
+        t.lt(90)
+        t.circle(190 - i, 90)
+        t.lt(18)
 
-t.speed(0)
+    t.penup()
+    t.goto(0, 300)
+    t.pendown()
+    t.write(f"Dear {name},", align="center", font=("Script MT Bold", 40, "italic"))
+    t.penup()
+    t.goto(0, 250)
+    t.write(f"{message}", align="center", font=("Script MT Bold", 40, "italic"))
 
-for i in range(150):
-    t.circle(190-i,90)
-    t.lt(90)
-    t.circle(190-i,90)
-    t.lt(18)
+    turtle.done()
+
+person_name = input("Enter the name of the person: ")
+person_message = input("Enter the message for them: ")
+draw_flower(person_name, person_message)
